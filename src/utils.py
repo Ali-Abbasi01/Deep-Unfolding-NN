2fcc10d5c26470ab09b0f67ab06fcf5dc7d7a733
Ali Abbasi <ali.abbasi@colorado.edu>
2025-05-13 18:25:58 -0600

    Add sum rate loss

--- src/utils.py
+++ src/utils.py
@@ -24,3 +24,31 @@ def calculate_sum_rate(H, V, alpha, sig):
             sum_rate += alpha[k][i] * R
 
     return sum_rate
+
+def sum_rate_loss_BC(H, V, PT):
+    # Recieves H as a dataframe of num_samples rows and num_users columns, V as a dict of num_samples keys, the value of each is a dict of num_users keys and their corresponding V as values. Outputs the average of sum rate across samples.
+    num_samples = len(H)
+    K = H.shape[1]
+    n_tx = H.iloc[0, 0].shape[1]
+    n_rx = [H.iloc[0, i].shape[0] for i in range(K)]
+    s_rate = []
+    for i in range(num_samples):
+        s = 0
+        for j in range(K):
+            ey = torch.eye(n_rx[j], dtype=torch.cfloat)
+            Omeg1 = 0
+            for k in range(K):
+                if k == j: pass
+                else:
+                    Omeg1 += V[i][k] @ V[i][k].conj().T
+            Omeg1 = H.iloc[i, j] @ Omeg1 @ H.iloc[i, j].conj().T
+            Omeg2 = 0
+            for k in range(K):
+                Omeg2 += torch.trace(V[i][k] @ V[i][k].conj().T)
+            Omeg2 = (1/PT) * Omeg2 * ey
+            Omeg = torch.linalg.inv(Omeg1 + Omeg2)
+            tmp = ey + H.iloc[i, j] @ V[i][j] @ V[i][j].conj().T @ H.iloc[i, j].conj().T @ Omeg
+            rate = (1/torch.log(torch.tensor(2.0))) * torch.logdet(tmp)
+            s += rate
+        s_rate.append(s)
+    return torch.tensor(s_rate).mean()
