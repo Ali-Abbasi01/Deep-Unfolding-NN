5f6775cddac667eaa9ebaaa10720981326212fe7
Ali Abbasi <ali.abbasi@colorado.edu>
2025-05-16 23:40:02 -0600

    Add unsupervised train

--- src/CNN.py
+++ src/CNN.py
@@ -66,3 +66,74 @@ class Trainer():
                 loss.backward()
                 optimizer.step()
             print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
+
+    def train_unsupervised(self, H_df, loss_fn, batch_size, lr=0.001):
+        
+        def srate_penalty_obj(H, V, alpha, P_max, penalty_coef):
+
+            # Bring H back to the dictionary form
+            K = int(H.shape[1]/2)
+            B, twoK, Nr, Nt = X.shape
+            assert twoK == 2*K, f"Expected 2*K={2*K} channels, got {twoK}"
+            
+            channel_dicts = []
+            for i in range(B):
+                sample_dict = {}
+                for k in range(K):
+                    real = H[i, 2*k    , :, :]   # [Nr, Nt]
+                    imag = H[i, 2*k + 1, :, :]   # [Nr, Nt]
+                    Hk   = torch.complex(real, imag)
+                    sample_dict[k] = Hk
+                channel_dicts.append(sample_dict)
+            H = channel_dicts
+
+            # Bring V back to the dictionary form
+            K = int(V.shape[1]/2)
+            B, twoK, Nr, Nt = V.shape
+            assert twoK == 2*K, f"Expected 2*K={2*K} channels, got {twoK}"
+            
+            V_dicts = []
+            for i in range(B):
+                sample_dict = {}
+                for k in range(K):
+                    real = V[i, 2*k    , :, :]   # [Nr, Nt]
+                    imag = V[i, 2*k + 1, :, :]   # [Nr, Nt]
+                    Vk   = torch.complex(real, imag)
+                    sample_dict[k] = Vk
+                V_dicts.append(sample_dict)
+            V = V_dicts
+
+            # Calculate the sum rate
+            s_rate = []
+            for b in range(B):
+                s_rate.append(calculate_sum_rate(H[b], V[b], alpha, sig))
+            
+            # Calculate penalties
+            pens = []
+            for b in range(B):
+                s_trace = 0
+                for k in range(K):
+                    s_trace += torch.linalg.trace(V[b][k] @ V[b][k].conj().T)
+                pen = penalty_coeff * ((s_trace - P_max).clamp(min=0) ** 2).mean()
+                pens.append(pen)
+
+            #total objective
+            loss = ((-1)*s_rate + pens).mean()
+            return loss
+
+        criterion = srate_penalty_obj()  # Using Mean Squared Error loss for regression
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+        # Training loop
+        num_epochs = 10
+        for epoch in range(num_epochs):
+            model.train()
+            for inputs, targets in dataloader:
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                loss.backward()
+                optimizer.step()
+            print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
+
+        print("Training complete!")
