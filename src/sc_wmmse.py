6eaefb60039491ea2d8ea71e7fa107f36acf9801
Ali Abbasi <ali.abbasi@colorado.edu>
2025-06-03 22:43:56 -0600

    Add wmmse for single cell

---
+++ src/sc_wmmse.py
@@ -0,0 +1,84 @@
+# The single cell WMMSE algorithm
+
+import torch
+
+class WMMSE_alg():
+    def __init__(self, K, n_tx, n_rx, H, PT, sig_k, d, alpha, max_iter_alg, tol_alg):
+        self.K = K
+        self.n_tx = n_tx
+        self.n_rx = n_rx
+        self.H = H
+        self.PT = PT
+        self.sig_k = sig_k
+        self.d = d
+        self.alpha = alpha
+        self.max_iter_alg = max_iter_alg
+        self.tol_alg = tol_alg
+
+    def algorithm(self, V_init):
+        def update_U(V):
+            U = {}
+            for k in range(self.K):
+                term1 = (self.sig_k[str(k)] / self.PT) * (sum([torch.trace(V[str(k)] @ V[str(k)].conj().T) for k in range(self.K)])) * torch.eye(self.n_rx[k], dtype=torch.cdouble)
+                term2 = sum([(self.H[str(k)] @ V[str(k)] @ V[str(k)].conj().T @ self.H[str(k)].conj().T)for k in range(self.K)])
+                A = term1 + term2
+                U[str(k)] = torch.linalg.inv(A) @ self.H[str(k)] @ V[str(k)]
+            return U
+        
+        def update_W(U, V):
+            W = {}
+            for k in range(self.K):
+                E = torch.eye(self.d[k], dtype=torch.cdouble) - U[str(k)].conj().T @ self.H[str(k)] @ V[str(k)]
+                W[str(k)] = torch.linalg.inv(E)
+            return W
+
+        def update_V(U, W):
+            V = {}
+            for k in range(self.K):
+                term1 = (sum([((self.sig_k[k] / self.PT) * torch.trace(self.alpha[str(k)] * U[str(k)] @ W[str(k)] @ U[str(k)].conj().T)) for k in range(self.K)])) * torch.eye(self.n_tx, dtype=torch.cdouble)
+                term2 = sum([(self.alpha[str(k)] * self.H[str(k)].conj().T @ U[str(k)] @ W[str(k)] @ U[str(k)].conj().T @ self.H[str(k)]) for k in range(self.K)])
+                B = term1 + term2
+                V[str(k)] = self.alpha[str(k)] * torch.linalg.inv(B) @ self.H[str(k)].conj().T @ U[str(k)] @ W[str(k)]
+            return V
+        
+        def proj_power(V):
+            alph = torch.sqrt(self.PT) / torch.sqrt(sum([torch.trace(V[str(k)] @ V[str(k)].conj().T) for k in range(self.K)]))
+            return alph * V
+        
+        # Keep record of V, U, and W
+        V_l = []
+        U_l = []
+        W_l = []
+
+        # The algorithm
+        U = update_U(V_init)
+        W = update_W(U, V)
+        V = update_V(U, W)
+        V_l.append(V)
+        U_l.append(U)
+        W_l.append(W)
+        for _ in range(self.max_iter_alg):
+            W_prm = W
+            U = update_U(V)
+            W = update_W(U, V)
+            V = update_V(U, W)
+            V_l.append(V)
+            U_l.append(U)
+            W_l.append(W)
+
+            # Check for convergance
+            val1 = 0
+            for k in range(self.K):
+                val1 += torch.log2(torch.linalg.det(W[str(k)]))
+
+            val2 = 0
+            for k in range(self.K):
+                val2 += torch.log2(torch.linalg.det(W_prm[str(k)]))
+
+            if torch.abs(val1 - val2) <= self.tol_alg:
+                break
+        
+        V_opt = proj_power(V)
+        
+        return V_opt, V_l, U_l, W_l
+
